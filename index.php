52433ad88676471ce286ad3439d5ea66b5de8614
bariscodefxy <bariscodefx@gmail.com>
2022-11-02 19:12:39 +0000

    kuantum sayıları

--- index.php
+++ index.php
@@ -5,3 +5,8 @@ echo h1("Kuantum Sayıları");
 echo span("n => 1,2,3...");
 endl();
 echo span("l => 0,1...(n-1)");
+endl();
+echo span("ml => -1...0...1");
+endl();
+echo span("ms => +1/2, -1/2");
+echo p("aralığında değer alırlar.");
