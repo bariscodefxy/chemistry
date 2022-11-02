70cb5220ecdb7df79530fa4aeb47985605bb7a36
bariscodefxy <bariscodefx@gmail.com>
2022-11-02 19:18:33 +0000

    elektron dizilimi

--- index.php
+++ index.php
@@ -10,3 +10,6 @@ echo span("ml => -1...0...1");
 endl();
 echo span("ms => +1/2, -1/2");
 echo p("aralığında değer alırlar.");
+
+echo h1("Elektron Dizilimi");
+echo p("1s² 2s² 2p² 3s² 3p⁶ 4s² 3d¹⁰... şeklinde gider.");
